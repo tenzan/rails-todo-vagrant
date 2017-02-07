0a85352ba90c5facab6dcc8d46c9a0df40435c67
Askar Karasaev <askar.karasaev@gmail.com>
2017-02-07 11:58:18 +0900

    first commit

---
+++ Vagrantfile
@@ -0,0 +1,41 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# All Vagrant configuration is done below. The "2" in Vagrant.configure
+# configures the configuration version (we support older styles for
+# backwards compatibility). Please don't change it unless you know what
+# you're doing.
+
+Vagrant.configure(2) do |config|
+
+  config.vm.box = "ubuntu-16.04-x64"
+
+  ENV['LC_ALL']="en_US.UTF-8"
+
+  # Disable automatic box update checking. If you disable this, then
+  # boxes will only be checked for updates when the user runs
+  # `vagrant box outdated`. This is not recommended.
+  config.vm.box_check_update = false
+  config.ssh.forward_agent = true
+
+  config.vm.synced_folder ".", "/vagrant", disabled: true
+  config.vm.synced_folder "~/apt-archives", "/var/cache/apt/archives/", create: true
+
+    config.vm.provider "virtualbox" do |v|
+      v.customize ["modifyvm", :id, "--memory", 2048]
+      v.customize ["modifyvm", :id, "--cpus", 2]
+    end
+
+    config.vm.define :api do |api|
+      api.vm.hostname = "api"
+      api.vm.network "private_network", ip: "10.20.30.100"
+      api.vm.provision "shell", path: "deployment/provision-api.sh", privileged: false
+    end
+
+    config.vm.define :ui do |ui|
+      ui.vm.hostname = "ui"
+      ui.vm.network "private_network", ip: "10.20.30.101"
+      # ui.vm.provision "shell", path: "deployment/provision-ui.sh", privileged: false
+    end
+
+  end
---
+++ deployment/provision-api.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+sudo sed -i'~' -E "s@http://(..\.)?(archive|security)\.ubuntu\.com/ubuntu@http://ftp.jaist.ac.jp/pub/Linux/ubuntu@g" /etc/apt/sources.list
+
+source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
+wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
+
+sudo apt-get -y update
+sudo apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev libreadline-dev rethinkdb >> /dev/null 2>&1
+
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"'               >> ~/.bashrc
+source ~/.bashrc
+
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
+sudo -H -u ubuntu bash -i -c 'rbenv install 2.4.0'
+sudo -H -u ubuntu bash -i -c 'rbenv global 2.4.0'
+sudo -H -u ubuntu bash -i -c 'rbenv rehash'
+sudo -H -u ubuntu bash -i -c 'gem install bundler --no-ri --no-rdoc'
+sudo -H -u ubuntu bash -i -c 'rbenv rehash'
+
+sudo -H -u ubuntu bash -i -c 'curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -'
+sudo -H -u ubuntu bash -i -c 'rbenv rehash'
+
+#  >> /dev/null 2>&1 is used to avoid messages: dpkg-preconfigure: unable to re-open stdin: No such file or directory
+sudo apt-get install -y nodejs >> /dev/null 2>&1
+
+sudo -H -u ubuntu bash -i -c 'gem install --no-ri --no-rdoc rails -v 5.0.1'
+
+cd
+
+git clone https://github.com/tenzan/rails-todo-api.git
+
+cd rails-todo-api
+
+sudo -H -u ubuntu bash -i -c 'rethinkdb &'
+sudo -H -u ubuntu bash -i -c 'bundle install'
+sudo -H -u ubuntu bash -i -c 'rails nobrainer:seed'
+sudo -H -u ubuntu bash -i -c 'rails s'
